9c14207f0ca4f5e4bd1a9f1ef2c4e875dc8926bd
Pradeep Yadav <119403808+Pradeepyadav210@users.noreply.github.com>
2024-01-26 13:35:03 +0530

    Add files via upload

---
+++ Hotel-Web/index.js
@@ -0,0 +1,6 @@
+const signUp = document.getElementById("submit");
+signUp.onclick = function(){open("signin.html")}
+
+const signIn = document.getElementById("submit1");
+signUp.onclick = function(){open("index.html")}
+
