ab08a3da920b9e059c08a87971fc9d782a65fa28
Pradeep Yadav <119403808+Pradeepyadav210@users.noreply.github.com>
2024-01-27 22:59:29 +0530

    marrige hall

--- Hotel-Web-1/index.js
+++
@@ -1,6 +0,0 @@
-const signUp = document.getElementById("submit");
-signUp.onclick = function(){open("signin.html")}
-
-const signIn = document.getElementById("submit1");
-signUp.onclick = function(){open("index.html")}
-
